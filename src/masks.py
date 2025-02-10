4d7024d7bea803cedda30e0f87090aa749840381
chiri <chirik2000xd@mail.ru>
2025-02-10 21:52:31 +0300

    Выполнил задание 12.2

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,35 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(filename)s - %(levelname)s - %(message)s',
+                    filename='../logs/masks_log.log',
+                    filemode='w')
+
+mask_card_number_logger = logging.getLogger()
+mask_account_logger = logging.getLogger()
+
 def get_mask_card_number(card_number: str) -> str:
-    """Функция принимает номер карты и выдает замаскированную версию"""
+    """Функция маскировки номера карты"""
+    mask_card_number_logger.info('Запуск маскировки номера карты')
     if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+        mask_card_number_logger.info('Маска номера карты создана')
+        return f"{card_number[0: 4]} {card_number[4: 6]}{"*" * 2} {"*" * 4} {card_number[-4:]}"
     else:
+        mask_card_number_logger.info('Неверные данные')
         return "Неверные данные"
 
 
 def get_mask_account(account_number: str) -> str:
-    """Функция принимает номер счета и выдает замаскированную версию"""
+    """Функция маскировки номера счета"""
+    mask_account_logger.info('Запуск маскировки номера счета')
     if account_number.isdigit() and len(account_number) == 20:
-        return f"**{account_number[-4:]}"
+        mask_account_logger.info('Маска номера счета создана')
+        return f"{"*" * 2}{account_number[-4::]}"
     else:
+        mask_account_logger.info('Неверные данные')
         return "Неверные данные"
 
-
-print(get_mask_card_number("1625344646464646"))
-print(get_mask_account("81726645454545454545"))
+# Пример вызова функций
+if __name__ == "__main__":
+    print(get_mask_card_number("1625344646464646"))
+    print(get_mask_account("81726645454545454545"))
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,29 @@
 import json
-import os
+import logging
+from json import JSONDecodeError
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(filename)s - %(levelname)s - %(message)s",
+    filename="../logs/utils.log",
+    filemode="w",
+)
+
+get_financial_transactions_logger = logging.getLogger()
 
 
-def load_transactions(file_path):
-    # Проверяем, существует ли файл и не пустой ли он
-    if not os.path.isfile(file_path) or os.path.getsize(file_path) == 0:
-        return []
-
+def get_financial_transactions(path: str) -> list:
+    """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
+    get_financial_transactions_logger.info("Начало работы функции")
     try:
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-            # Проверяем, является ли загруженные данные списком
-            if isinstance(data, list):
-                return data
-            else:
+        with open(path, encoding="utf-8") as file:
+            try:
+                transactions = json.load(file)
+                get_financial_transactions_logger.info("Создан список словарей финансовых транзакций")
+                return transactions
+            except JSONDecodeError:
+                get_financial_transactions_logger.error("Ошибка файла с транзакциями")
                 return []
-    except json.JSONDecodeError:
-        return []  # Возвращаем пустой список в случае ошибки парсинга JSON
-    except Exception:
-        return []  # Возвращаем пустой список в случае других ошибок
+    except FileNotFoundError:
+        get_financial_transactions_logger.error("Файл с транзакциями не найден")
+        return []
